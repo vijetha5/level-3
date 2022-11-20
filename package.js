0335d84bc67e73ad83be6d3c89699b4e1f5ca45d
vijetha5 <113460328+vijetha5@users.noreply.github.com>
2022-11-20 22:37:58 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,22 @@
+{
+    "name": "hello-node",
+    "version": "1.0.0",
+    "description": "",
+    "main": "index.js",
+    "scripts": {
+      "start": "node index.js",
+      "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "author": "",
+    "license": "ISC",
+    "devDependencies": {
+      "eslint": "^8.23.1",
+      "eslint-config-standard": "^17.0.0",
+      "eslint-plugin-import": "^2.26.0",
+      "eslint-plugin-n": "^15.2.5",
+      "eslint-plugin-promise": "^6.0.1"
+    },
+    "dependencies": {
+      "minimist": "^1.2.6"
+    }
+  } 
